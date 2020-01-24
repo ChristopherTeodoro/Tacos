5deb3249a22552c184e0f188975f24cd86afc45b
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-24 10:25:13 -0600

    big push

---
+++ controllers/tacos_controller.js
@@ -0,0 +1,37 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+var taco = require("../models/taco.js");
+
+// Our GET request to grab database contents
+router.get("/", function(req, res) {
+	taco.selectAll(function(data) {
+		var hbsObject = {
+			tacos: data
+		};
+
+		res.render("index", hbsObject);
+	});
+});
+
+// Our POST request to add a taco to the database
+router.post("/", function(req, res) {
+	console.log(req.body.taco_name);
+	if(req.body.taco_name !== "") {
+		taco.insertOne(req.body.taco_name.trim(), function() {
+			res.redirect("/");
+		});
+	}
+});
+
+// Our PUT request to update a taco's status
+router.put("/:id", function(req, res) {
+	console.log(req.params.id);
+
+	taco.updateOne(req.params.id, function() {
+		res.redirect("/");
+	});
+})
+
+// Export
+module.exports = router;
---
+++ models/taco.js
@@ -0,0 +1,24 @@
+// Dependencies
+var orm = require("../config/orm.js");
+
+// We pass in query parameters as required by our ORM and also a callback to receive data
+var taco = {
+	selectAll: function(cb) {
+		orm.selectAll("tacos", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(tacoName, cb) {
+		orm.insertOne("tacos", "taco_name", tacoName, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(tacoId, cb) {
+		orm.updateOne("burgers", "devoured", 1, "id", tacoId, function(res) {
+			cb(res);
+		});
+	}
+};
+
+// Export
+module.exports = taco;
