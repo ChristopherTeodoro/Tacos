fbab997bc6920bd5a1029e0ec89b3a55251bdbb4
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-24 09:42:22 -0600

    creating db and insertions

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+### Populate data with 3 starter tacos
+INSERT INTO tacos (taco_name) VALUES ('Bacon Egg and Cheese Taco');
+
+INSERT INTO tacos (taco_name) VALUES ('Potato and Egg Taco');
+
+INSERT INTO tacos (taco_name) VALUES ('Bean and Cheese Taco');
